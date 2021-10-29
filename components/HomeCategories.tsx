a608dc79e83c495138fffe9957dab3a7422db0f2
Nabavi2 <rohullah.ghaznavi78@gmail.com>
2021-10-29 21:05:21 +0430

    add homecategories

---
+++ components/HomeCategories.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { StyleSheet, Image, FlatList, Pressable, TouchableOpacity } from 'react-native';
+import categories from '../data/categories';
+import { View, Text } from './../components/Themed';
+import Navigation from '../navigation/index';
+import { useNavigation } from '@react-navigation/native';
+import movie from '../data/movie';
+import MovieDetailScreen from '../screens/MovieDetailScreen';
+interface HomeCategoryScreen {
+    category: {
+        id: string,
+        title: string,
+        movies: {
+            id: string,
+            poster: string
+        }[]
+    }
+};
+function HomeCategories(props: HomeCategoryScreen,) {
+    const { category } = props;
+    const navigation = useNavigation();
+    // const onPress = (movie: { id: any; poster?: string; }) => {
+    //     navigation.navigate('MovieDetailScreen', { id: movie.id });
+    // }
+    let i = 0;
+    return (
+
+        <>
+            <View style={styles.container}>
+
+                <View style={{ backgroundColor: 'black', marginBottom: 10 }}>
+                    <Text style={{ margin: 10, }}>
+                        {category.title}
+
+                    </Text>
+
+                    <FlatList
+                        data={category.movies}
+                        renderItem={({ item }) => (
+                            // <Pressable onPress={() => navigation.navigate('MovieDetailScreen')}>
+                            //     </Pressable>
+                            <Image style={styles.image} source={{ uri: item.poster }} />
+
+                        )
+                        }
+                        horizontal
+                    />
+
+                </View>
+
+            </View>
+        </>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 8,
+    },
+    image: {
+        width: 120,
+        height: 200,
+        borderRadius: 7,
+        margin: 8
+    }
+})
+export default HomeCategories;
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -4,19 +4,18 @@ import categories from '../data/categories';
 import movie from '../data/movie';
 import { View, Text } from './../components/Themed';
 import { AntDesign, MaterialIcons, Ionicons } from '@expo/vector-icons';
-// import HomeCategories from '../components/HomeCategories';
-// const firstCat = categories.items[0];
-// const secondCat = movie.seasons.items[0].episodes.items[0];
+import HomeCategories from '../components/HomeCategories';
 function HomeScreen() {
 
     return (
         <View style={styles.container}>
-            {/* <FlatList
-                data={firstCat.movies}
+
+            <FlatList
+                data={categories.items}
                 renderItem={({ item }) => (
-                    <HomeCategories category={firstCat} />
-                    )}
-            /> */}
+                    <HomeCategories category={item} />
+                )}
+            />
             <Text>this is home Screen</Text>
 
 
