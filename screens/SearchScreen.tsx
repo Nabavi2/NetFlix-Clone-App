b95b2b9ef4f1202586d18b14e7e5e221abc94688
Nabavi2 <rohullah.ghaznavi78@gmail.com>
2021-11-28 09:05:07 +0430

    edit search screen

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -1,79 +1,79 @@
-// import React, { useCallback, useEffect, useState } from 'react';
-// import { View, Text, ActivityIndicator, FlatList, TextInput, Image, Button, StyleSheet } from 'react-native';
-// import { SearchBar } from 'react-native-elements'
-// import { TouchableOpacity } from 'react-native-gesture-handler';
-// import { useDispatch, useSelector } from 'react-redux';
-// import * as movieActions from '../store/actions/movie';
-// import * as seriesActions from '../store/actions/series';
-// function SearchScreen() {
+import React, { useCallback, useEffect, useState } from 'react';
+import { View, Text, ActivityIndicator, FlatList, TextInput, Image, Button, StyleSheet } from 'react-native';
+import { SearchBar } from 'react-native-elements'
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { useDispatch, useSelector } from 'react-redux';
+import * as movieActions from '../store/actions/movie';
+import * as seriesActions from '../store/actions/series';
+function SearchScreen() {
 
 
-//     const movies = useSelector((state) => state.movies.availableMovies);
-//     const series = useSelector((state) => state.series.availableSeries);
+    const movies = useSelector((state) => state.movies.availableMovies);
+    const series = useSelector((state) => state.series.availableSeries);
 
-//     const [filterData, setFilterData] = useState(movies);
-//     const [search, setSearch] = useState();
-//     const [isLoading, setIsLoading] = useState(false);
-//     const [error, setError] = useState(null);
+    const [filterData, setFilterData] = useState(movies);
+    const [search, setSearch] = useState();
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
 
 
-//     const searchFilterFunction = (text: any) => {
-//         if (text) {
-//             const newData = filterData.filter((item: any) => {
-//                 const itemData = item.title ? item.title.toUpperCase() : ''.toUpperCase();
-//                 const textData = text.toUpperCase();
-//                 return itemData.indexOf(textData) > -1;
-//             });
-//             setFilterData(newData);
-//             setSearch(text);
+    const searchFilterFunction = (text: any) => {
+        if (text) {
+            const newData = filterData.filter((item: any) => {
+                const itemData = item.title ? item.title.toUpperCase() : ''.toUpperCase();
+                const textData = text.toUpperCase();
+                return itemData.indexOf(textData) > -1;
+            });
+            setFilterData(newData);
+            setSearch(text);
 
-//         } else {
-//             setFilterData(movies);
-//             setSearch(text)
-//         }
-//     };
-//     return (
-//         <View style={{ flex: 1 }}>
-//             <SearchBar
-//                 placeholder="Search..."
-//                 onChangeText={(text) => searchFilterFunction(text)}
-//                 autoCorrect={true}
-//                 value={search}
-//             />
-//             {/* <View style={{ flexDirection: 'row-reverse', width: '100%', marginTop: 20, marginLeft: 10, marginRight: 10, }}>
-//             </View> */}
-//             <FlatList
-//                 data={filterData}
-//                 key={filterData.id}
-//                 renderItem={({ item }) => {
-//                     return (
-//                         <View style={{ flexDirection: 'row-reverse', width: "90%", marginTop: 20, }}>
-//                             <Image
-//                                 style={styles.image}
-//                                 source={{ uri: item.poster }} />
-//                             <Text style={{ color: '#000', fontSize: 24, padding: 10, }}>{item.title}</Text>
-//                         </View>
-//                     )
-//                 }}
-//                 style={{ marginLeft: 120, }}
-//             />
-//         </View>
-//     );
-// }
-// const styles = StyleSheet.create({
-//     image: {
-//         marginLeft: 10,
-//         marginBottom: 10,
-//         width: 60,
-//         height: 60,
-//         borderRadius: 30,
-//         borderColor: '#c75a5f',
-//         resizeMode: 'cover',
-//     },
-//     input: {
-//         borderColor: 'grey',
-//         width: '96%',
-//         borderWidth: 1,
-//     }
-// })
-// export default SearchScreen;
+        } else {
+            setFilterData(movies);
+            setSearch(text)
+        }
+    };
+    return (
+        <View style={{ flex: 1 }}>
+            <SearchBar
+                placeholder="Search..."
+                onChangeText={(text) => searchFilterFunction(text)}
+                autoCorrect={true}
+                value={search}
+            />
+            {/* <View style={{ flexDirection: 'row-reverse', width: '100%', marginTop: 20, marginLeft: 10, marginRight: 10, }}>
+            </View> */}
+            <FlatList
+                data={filterData}
+                key={filterData.id}
+                renderItem={({ item }) => {
+                    return (
+                        <View style={{ flexDirection: 'row-reverse', width: "90%", marginTop: 20, }}>
+                            <Image
+                                style={styles.image}
+                                source={{ uri: item.poster }} />
+                            <Text style={{ color: '#000', fontSize: 24, padding: 10, }}>{item.title}</Text>
+                        </View>
+                    )
+                }}
+                style={{ marginLeft: 120, }}
+            />
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    image: {
+        marginLeft: 10,
+        marginBottom: 10,
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        borderColor: '#c75a5f',
+        resizeMode: 'cover',
+    },
+    input: {
+        borderColor: 'grey',
+        width: '96%',
+        borderWidth: 1,
+    }
+})
+export default SearchScreen;
