673c26993370e03a2e5d1ddd607cbcd124f4d177
Nabavi2 <rohullah.ghaznavi78@gmail.com>
2021-11-28 08:30:51 +0430

    add series categoris in componants

--- screens/ComingSoonScreen.tsx
+++ screens/ComingSoonScreen.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import HomeScreen from './HomeScreen';
-function ComingSoonScreen() {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', }}>
-            <Text style={{ fontSize: 22, color: 'blue', fontWeight: 'bold' }}> The  NetFlix Movie and series trailer </Text>
-            <HomeScreen />
-        </View>
-    );
-}
+// import React from 'react';
+// import { Text, View } from 'react-native';
+// import HomeScreen from './HomeScreen';
+// function ComingSoonScreen() {
+//     return (
+//         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', }}>
+//             <Text style={{ fontSize: 22, color: 'blue', fontWeight: 'bold' }}> The  NetFlix Movie and series trailer </Text>
+//             <HomeScreen />
+//         </View>
+//     );
+// }
 
-export default ComingSoonScreen;
+// export default ComingSoonScreen;
