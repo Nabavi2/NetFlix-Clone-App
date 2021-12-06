bb59fcb1cbd9d10db787f7cbb7c619b8e2bd84d6
Hamid313-Coder <hamid.hussainy0@gmail.com>
2021-12-06 09:36:10 +0430

    fixing the conflicts of video play

--- components/EpisodeItems.tsx
+++ components/EpisodeItems.tsx
@@ -1,62 +1,71 @@
-import React from 'react';
-import { StyleSheet, Image, FlatList, Pressable, TouchableOpacity } from 'react-native';
-import { View, Text } from './../components/Themed';
-import Navigation from '../navigation/index';
-import { useNavigation } from '@react-navigation/native';
-import movie from '../data/movie';
-import { Episode } from '../types';
-import MovieDetailScreen from '../screens/MovieDetailScreen';
-import { AntDesign } from '@expo/vector-icons';
+import React from "react";
+import {
+  StyleSheet,
+  Image,
+  FlatList,
+  Pressable,
+  TouchableOpacity,
+} from "react-native";
+import { View, Text } from "./../components/Themed";
+import Navigation from "../navigation/index";
+import { useNavigation } from "@react-navigation/native";
+import movie from "../data/movie";
+import { Episode } from "../types";
+import MovieDetailScreen from "../screens/MovieDetailScreen";
+import { AntDesign } from "@expo/vector-icons";
 
 interface EpisodesItem {
-    episode: Episode;
-    onPress: (episode: any) => {};
-};
+  episode: Episode;
+  onPress: (episode: any) => {};
+}
 function EpisodeItems(props: EpisodesItem) {
-    const { episode, onPress } = props;
+  const { episode, onPress } = props;
 
-    return (
-        <Pressable onPress={() => onPress}
-            style={{ flex: 1, padding: 8, backgroundColor: '#000' }}>
-            <View style={styles.container}>
-                <Image
-                    style={styles.imageView}
-                    source={{ uri: episode.poster }} />
-                <View style={styles.rowContainer}>
-                    <Text style={{ fontSize: 12, color: '#FFF' }}>{episode.title} part 1 and 2  </Text>
-                    <Text style={{ fontSize: 10, color: '#968d8c' }}> {episode.duration} </Text>
-                </View>
-                <AntDesign name="download" size={24} color="#FFF" />
-            </View>
-            <Text style={{ marginHorizontal: 10, marginBottom: 10, color: '#FFF' }}> The Professor enjoys the spoils of his latest trick.
-                Río is disturbed by news he sees on the television. {episode.plot}Mónica's condition worsens.
-            </Text>
-        </Pressable>
-
-    );
+  return (
+    <Pressable
+      onPress={() => onPress}
+      style={{ flex: 1, padding: 8, backgroundColor: "#000" }}
+    >
+      <View style={styles.container}>
+        <Image style={styles.imageView} source={{ uri: episode.poster }} />
+        <View style={styles.rowContainer}>
+          <Text style={{ fontSize: 12, color: "#FFF" }}>{episode.title}</Text>
+          <Text style={{ fontSize: 10, color: "#968d8c" }}>
+            {" "}
+            {episode.duration}{" "}
+          </Text>
+        </View>
+        <AntDesign name="download" size={24} color="#FFF" />
+      </View>
+      <Text style={{ marginHorizontal: 10, marginBottom: 10, color: "#FFF" }}>
+        {" "}
+        {episode.plot}
+      </Text>
+    </Pressable>
+  );
 }
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        marginHorizontal: 10,
-        marginBottom: 5,
-        alignItems: 'center',
-        backgroundColor: '#000',
-    },
-    rowContainer: {
-        width: 150,
-        height: 100,
-        justifyContent: 'center',
-        backgroundColor: '#000',
-        marginHorizontal: 8,
-    },
-    imageView: {
-        width: 130,
-        aspectRatio: 16 / 9,
-        resizeMode: 'cover',
-        borderRadius: 7,
-        alignItems: 'center',
-        marginRight: 10,
-    }
-})
+  container: {
+    flexDirection: "row",
+    marginHorizontal: 10,
+    marginBottom: 5,
+    alignItems: "center",
+    backgroundColor: "#000",
+  },
+  rowContainer: {
+    width: 150,
+    height: 100,
+    justifyContent: "center",
+    backgroundColor: "#000",
+    marginHorizontal: 8,
+  },
+  imageView: {
+    width: 130,
+    aspectRatio: 16 / 9,
+    resizeMode: "cover",
+    borderRadius: 7,
+    alignItems: "center",
+    marginRight: 10,
+  },
+});
 export default EpisodeItems;
